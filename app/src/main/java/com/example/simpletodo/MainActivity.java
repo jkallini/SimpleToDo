88f103d7c26cb0056d1196dcf25cb0d6685f2030
Julie Kallini <juliekallini@fb.com>
2019-06-25 16:33:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.simpletodo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // declaring stateful objects here; these will be null before onCreate is called
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // obtain a reference to the ListView created with the layout
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        // initialize the items list
+        readItems();
+        // initialize the adapter using the items list
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        // wire the adapter to the view
+        lvItems.setAdapter(itemsAdapter);
+
+        // add some mock items to the list
+        //items.add("First todo item");
+        //items.add("Second todo item");
+
+        // setup the listener on creation
+        setupListViewListener();
+    }
+
+    private void setupListViewListener() {
+        Log.i("Main Activity", "Setting up listener on list view");
+        // set the ListView's itemLongClickListener
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Main Activity", "Item: removed from list: " + position);
+                // remove the item in the list at the index given by position
+                items.remove(position);
+                // notify the adapter that the underlying dataset changed
+                itemsAdapter.notifyDataSetChanged();
+                // store the updated list
+                writeItems();
+                // return true to tell the framework that the long click was consumed
+                return true;
+            }
+        });
+    }
+
+    public void onAddItem(View v) {
+        // obtain a reference to the EditText created with the layout
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        // grab the EditText's content as a String
+        String itemText = etNewItem.getText().toString();
+        // add the item to the list via the adapter
+        itemsAdapter.add(itemText);
+        // clear the EditText by setting it to an empty String
+        etNewItem.setText("");
+        // display a notification to the user
+        writeItems();
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+    // returns the file in which the data is stored
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    // read the items from the file system
+    private void readItems() {
+        try {
+            // create the array using the content in the file
+            items = new ArrayList<String>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            // Log the error
+            Log.e("Main Activity", "Error reading file", e);
+            // just load an empty list
+            items = new ArrayList<>();
+        }
+    }
+
+    // write the items to the filesystem
+    private void writeItems() {
+        try {
+            // save the item list as a line-delimited text file
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("Main Activity", "Error writing file", e);
+        }
+    }
+}
+
